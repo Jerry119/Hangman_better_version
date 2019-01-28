38ca01c8aa90f8ff75d299e2fb1bb1018b27a73e
Jerry119 <liu.jerry55@gmail.com>
2019-01-28 12:54:23 -0800

    modify main.py, index.html and playing.html

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import random
 
 app = Flask(__name__)
 
+word = ""
+hint = ""
+old_guess = ""
+
 @app.route('/')
 @app.route('/<best_score>')
 def home(best_score=0):
@@ -10,8 +14,12 @@ def home(best_score=0):
     lines = f.read().split('\n')
     n = random.randint(0, len(lines)-1)
     line = lines[n]
+    global word
     word = line.split('?')[0]
+    global hint
     hint = line.split('?')[1]
+    global old_guess
+    old_guess = ""
     f.close()
 
     return render_template("index.html", word=word, remain_cnt=5, best_score=best_score, hint=hint)
@@ -20,10 +28,9 @@ def home(best_score=0):
 def in_game():
     remain_cnt = int(request.form['remain_cnt'])
     char = request.form['char'].lower()
-    word = request.form['word']
-    old_guess = request.form['guess']
     best_score = int(request.form['best_score'])
-    hint = request.form['hint']
+    
+    global old_guess
     
     if old_guess == "":
         for c in word:
@@ -40,16 +47,17 @@ def in_game():
             else:
                 new_guess += old_guess[i]
 
+        old_guess = new_guess
+
         if '_' not in new_guess:
             if remain_cnt > best_score:
                 best_score = remain_cnt
             return render_template("win.html", word=word, remain_cnt=remain_cnt, best_score=best_score, hint=hint)
     else:
-        new_guess = old_guess
         remain_cnt -= 1
         if remain_cnt == 0:
             return render_template("fail.html", word=word, remain_cnt=remain_cnt, best_score=best_score, hint=hint)
     
-    return render_template("playing.html", remain_cnt=remain_cnt, word=word, guess=new_guess, best_score=best_score, hint=hint)
+    return render_template("playing.html", remain_cnt=remain_cnt, guess=old_guess, best_score=best_score, hint=hint)
 
 
